f66942594a45a9a5f667cfb2bea99fc7b96c5209
ebistu <127283670+ebistu@users.noreply.github.com>
2024-07-14 20:55:03 +0300

    solved 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if(times < 0){
+        return "ERROR";
+    }
+    let returnStr = "";
+    for(i = 0; i < times; i++){
+        returnStr = returnStr + str; 
+    }
+    return returnStr; 
 };
 
 // Do not edit below this line
